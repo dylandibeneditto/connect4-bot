f3a4f6ea785bd20fd7bc8b8a4215b638b18b5454
dylan <justbeawesome9@gmail.com>
2024-02-14 20:15:48 -0500

    board visualizer

--- board.h
+++ board.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 namespace B
@@ -6,9 +7,28 @@ namespace B
     class Board
     {
     public:
-        vector<int> position;
+        bool position[49] = {0};
+        bool mask[49] = {0};
+        //0 - yellow, 1 - red
+        bool turn = 0;
+        bool canMove(int pos) {
+            return 1;
+        }
+
         void move(int pos) {
-            position.push_back(pos);
+            if(canMove(pos)==1) {
+                int h = 0;
+                while(mask[(7*pos)+h]==1) {
+                    h++;
+                }
+                mask[(7*pos)+h] = 1;
+
+                if(turn==0) { // if turn is yellow
+                    position[(7*pos)+h] = 1;
+                }
+
+                turn=!turn;
+            }
         }
     };
 }
--- main.cpp
+++ main.cpp
@@ -7,10 +7,21 @@ using namespace std;
 int main()
 {
     Board board;
-    board.move(1);
-    board.move(5);
-    board.move(3);
+    for (int i = 0; i < 100; i++)
+    {
+        int move;
+        cin >> move;
+        board.move(move);
+        for (int a = 6; a >= 0; a--)
+        {
+            for (int b = 0; b < 7; b++)
+            {
+                cout << ((board.mask[(7 * b) + a] + board.position[(7 * b) + a])==2 ? "x" : ((board.mask[(7 * b) + a] + board.position[(7 * b) + a])==1 ? "o" : "."));
+            }
+            cout << endl;
+        }
+        cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    }
 
-    
     return 0;
 }
