7f4e2d0682e66a392096601cbdd1ec9bb0b1990d
dylan <justbeawesome9@gmail.com>
2024-02-15 19:08:36 -0500

    feat: change of format settings and display blanks as " "

--- board.h
+++ board.h
@@ -1,44 +1,37 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-namespace B
-{
-    class Board
-    {
-    public:
-        bool position[49] = {0};
-        bool mask[49] = {0};
-        // 0 - yellow, 1 - red
-        bool turn = 0;
-        bool canMove(int pos)
-        {
-            return mask[(7 * pos) + 6] == 0 ? 1 : 0;
-        }
+namespace B {
+class Board {
+   public:
+    static const int WIDTH = 7;
+    static const int HEIGHT = 6;
+    bool position[(HEIGHT + 1) * WIDTH] = {0};
+    bool mask[(HEIGHT + 1) * WIDTH] = {0};
+    // 0 - yellow, 1 - red
+    bool turn = 0;
+    bool canMove(int pos) { return mask[(WIDTH * pos) + HEIGHT] == 0 ? 1 : 0; }
 
-        void move(int pos)
-        {
-            if (canMove(pos) == 1)
-            {
-                int h = 0;
-                while (mask[(7 * pos) + h] == 1)
-                {
-                    h++;
-                }
-                mask[(7 * pos) + h] = 1;
-
-                if (turn == 0)
-                { // if turn is yellow
-                    position[(7 * pos) + h] = 1;
-                }
-
-                turn = !turn;
-                cout << (turn == 0 ? "yellow's turn\n" : "red's turn\n");
+    void move(int pos) {
+        if (canMove(pos) == 1) {
+            int h = 0;
+            while (mask[(WIDTH * pos) + h] == 1) {
+                h++;
             }
-            else
-            {
-                cout << "invalid move!\n";
+            mask[(WIDTH * pos) + h] = 1;
+
+            if (turn == 0) {  // if turn is yellow
+                position[(WIDTH * pos) + h] = 1;
             }
+
+            turn = !turn;
+            cout << (turn == 0 ? "yellow's turn\n" : "red's turn\n");
+        } else {
+            cout << "invalid move!\n";
         }
-    };
-}
+    }
+
+    bool isWinningMove(int col) {}
+};
+}  // namespace B
--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
+
 #include "./board.h"
 
 using namespace B;
 using namespace std;
 
-int main()
-{
+int main() {
     Board board;
     int move;
-    for (int i = 0; i < 100; i++)
-    {
-        for (int a = 6; a >= 0; a--)
-        {
-            for (int b = 0; b < 7; b++)
-            {
-                cout << ((board.mask[(7 * b) + a] + board.position[(7 * b) + a]) == 2 ? "x" : ((board.mask[(7 * b) + a] + board.position[(7 * b) + a]) == 1 ? "o" : "."));
+    for (int i = 0; i < 100; i++) {
+        for (int a = 6; a >= 0; a--) {
+            for (int b = 0; b < 7; b++) {
+                cout << ((board.mask[(7 * b) + a] + board.position[(7 * b) + a]) == 2 ? "x" : ((board.mask[(7 * b) + a] + board.position[(7 * b) + a]) == 1 ? "o" : " "));
             }
             cout << endl;
         }
-        cout << "0123456\n";
+        cout << "1234567\n";
         cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
         cin >> move;
-        board.move(move);
+        board.move(move - 1);
     }
 
     return 0;
