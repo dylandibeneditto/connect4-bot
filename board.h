e3548313313d9fb48d90f0b36c78a1b0e2cdaf81
dylan <justbeawesome9@gmail.com>
2024-02-16 22:46:09 -0500

    feat: nmax algorithm and best move function

--- board.h
+++ board.h
@@ -1,12 +1,11 @@
-#include <cstdlib>
 #include <iostream>
 using namespace std;
 
 namespace B {
 class Board {
    public:
-    static const int WIDTH = 7;
-    static const int HEIGHT = 6;
+    static const int WIDTH = 4;
+    static const int HEIGHT = 3;
 
     /* BITMAP REFERENCE MAP */
     /*
@@ -30,11 +29,12 @@ class Board {
         ..oxxo.   0001100   0011110
     */
 
-    bool position[(HEIGHT + 1) * WIDTH] = {0};
-    bool mask[(HEIGHT + 1) * WIDTH] = {0};
+    mutable bool position[(HEIGHT + 1) * WIDTH] = {0};
+    mutable bool mask[(HEIGHT + 1) * WIDTH] = {0};
     // 0 - x, 1 - o
     bool turn = 0;
-    bool canMove(int pos) { return mask[(WIDTH * pos) + HEIGHT] == 0 ? 1 : 0; }
+    int moves = 0;
+    bool canMove(int pos) const { return position[(WIDTH * pos) + HEIGHT] == 0 ? true : false; }
 
     void move(int pos) {
         if (canMove(pos) == 1) {
@@ -49,12 +49,14 @@ class Board {
             }
 
             turn = !turn;
+            moves++;
         } else {
             cout << "invalid move!\n";
         }
     }
 
-    bool isWinningMove(int col) {
+    /* FIX THE WHILE LOOPS CHECKING RESERVED TOP ROW */
+    bool isWinningMove(int col) const {
         int h = 0;
         while (position[(WIDTH * col) + h] == 1) {
             h++;
@@ -66,45 +68,107 @@ class Board {
             int val = index;
             if (turn == 0) {  // if turn will be x
                 while (mask[val + kernel[i]] == 1) {
+                    if(val+kernel[i]>WIDTH*(HEIGHT+1))
+                        break;
+                    if(val)
                     val += kernel[i];
                     count++;
                 }
                 val = index;
                 while (mask[val - kernel[i]] == 1) {
+                    if(val-kernel[i]<0)
+                        break;
                     val -= kernel[i];
                     count++;
                 }
             } else {  // if turn will be o
                 while (position[val + kernel[i]] - mask[val + kernel[i]] == 1) {
+                    if(val+kernel[i]>WIDTH*(HEIGHT+1))
+                        break;
                     val += kernel[i];
                     count++;
                 }
                 val = index;
                 while (position[val + kernel[i]] - mask[val - kernel[i]] == 1) {
+                    if(val-kernel[i]<0)
+                        break;
                     val -= kernel[i];
                     count++;
                 }
             }
-            if (count >= 3) { // count will be three in a row as the kernel center is never factored
-                cout<<kernel[i];
-                return 1;
+            if (count >= 3) {  // count will be three in a row as the kernel center is never factored
+                cout << kernel[i];
+                return true;
             }
         }
 
-        return 0;
+        return false;
     }
 
-    int bestMove() {
-        int r = 0;
-        while (r > WIDTH == 0) {
-            if (isWinningMove(r) == 1) {
-                cout<<r;
-                return r;
-            } else {
-                r++;
-            }
+    int nmax(const Board& P, int alpha, int beta) {
+        if (P.moves == WIDTH * HEIGHT) // draw
+            return 0;
+
+        for (int x = 0; x < WIDTH; x++) { // win
+            cout << P.moves;
+            if (P.isWinningMove(x))
+                return (WIDTH * HEIGHT + (1 - P.moves)) / 2;
         }
-        return rand() % WIDTH;
+
+        int max = (WIDTH * HEIGHT - 1 - P.moves) / 2; // cant win immediately
+        if (beta > max) {
+            beta = max;                      
+            if (alpha >= beta) return beta;  
+        }
+
+        for (int x = 0; x < WIDTH; x++) // compute all possible moves
+            if (P.canMove(x)) {
+                Board B2(P.position, P.mask, P.moves, P.turn);
+                B2.move(x);
+                int score = -nmax(B2, -beta, -alpha);
+
+                if (score >= beta) return score;
+                if (score > alpha) alpha = score;                                                   
+            }
+        cout << endl;
+        return alpha;
+    }
+
+    bool canWinNext() const {
+        for (int i = 0; i < WIDTH; i++) {
+            if (isWinningMove(i)) return true;
+        }
+        return false;
+    }
+
+    int bestMove(Board& P) {
+        if (P.canWinNext())
+            return (WIDTH * HEIGHT + 1 - P.moves) / 2;
+        int min = -(WIDTH * HEIGHT - P.moves) / 2;
+        int max = (WIDTH * HEIGHT + 1 - P.moves) / 2;
+
+        while (min < max) {
+            int med = min + (max - min) / 2;
+            if (med <= 0 && min / 2 < med)
+                med = min / 2;
+            else if (med >= 0 && max / 2 > med)
+                med = max / 2;
+            int r = nmax(P, med, med + 1);
+            if (r <= med)
+                max = r;
+            else
+                min = r;
+        }
+        return min;
+    }
+
+    Board(bool position[25] = {0}, bool mask[25] = {0}, int moves = 0, bool turn = 0) {
+        for (int i = 0; i < 25; ++i) {
+            this->position[i] = position[i];
+            this->mask[i] = mask[i];
+        }
+        this->moves = moves;
+        this->turn = turn;
     }
 };
 }  // namespace B
--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@ using namespace B;
 using namespace std;
 
 void visualize(Board board) {
-            for (int a = 6; a >= 0; a--) {
-            for (int b = 0; b < 7; b++) {
-                cout << ((board.mask[(7 * b) + a] + board.position[(7 * b) + a]) == 2 ? " \033[1;43m \033[0m" : ((board.mask[(7 * b) + a] + board.position[(7 * b) + a]) == 1 ? " \033[1;41m \033[0m" : "  "));
-            }
-            cout << endl;
-            cout << endl;
+    for (int a = board.HEIGHT; a >= 0; a--) {
+        for (int b = 0; b < board.WIDTH; b++) {
+            cout << ((board.mask[(board.WIDTH * b) + a] + board.position[(board.WIDTH * b) + a]) == 2 ? " \033[1;33m█\033[0m" : ((board.mask[(board.WIDTH * b) + a] + board.position[(board.WIDTH * b) + a]) == 1 ? " \033[1;31m█\033[0m" : "  "));
         }
-        cout << " 1 2 3 4 5 6 7\n";
-        cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n";
+        cout << endl;
+        cout << endl;
+    }
+    cout << " 1 2 3 4 5 6 7\n";
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
 
 int main() {
@@ -23,10 +23,11 @@ int main() {
     for (int i = 0; i < 100; i++) {
         visualize(board);
         cin >> move;
-        if (board.canMove(move - 1) == 1) {
-            if (board.isWinningMove(move - 1) == 0) {
+        if (board.canMove(move - 1)) {
+            cout << "DEBUG";
+            if (!board.isWinningMove(move - 1)) {
                 board.move(move - 1);
-                board.move(board.bestMove());
+                board.move(board.bestMove(board));
             } else {
                 board.move(move - 1);
                 visualize(board);
